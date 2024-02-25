6e8e5e650f8431d1dc784080096509e9e96442f3
Dmitry Tyumentsev <tyumentsevde@gmail.com>
2024-02-25 16:00:21 +0300

    WP: 1st Converter

--- app/src/main/java/ru/ac/uniyar/tyumentsev/unitconverter/ui/converter/ConverterFragment.kt
+++ app/src/main/java/ru/ac/uniyar/tyumentsev/unitconverter/ui/converter/ConverterFragment.kt
@@ -2,13 +2,18 @@ package ru.ac.uniyar.tyumentsev.unitconverter.ui.converter
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import androidx.core.widget.doOnTextChanged
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import ru.ac.uniyar.tyumentsev.unitconverter.R
 import ru.ac.uniyar.tyumentsev.unitconverter.databinding.ConverterFragmentBinding
 
@@ -19,34 +24,102 @@ class ConverterFragment : Fragment() {
     private lateinit var binding: ConverterFragmentBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
         binding = DataBindingUtil.inflate(
-            inflater,
-            R.layout.converter_fragment,
-            container,
-            false
+            inflater, R.layout.converter_fragment, container, false
         )
         viewModel = ViewModelProvider(this)[ConverterViewModel::class.java]
 
-        val adapter = ArrayAdapter(
-            requireContext(),
+        val converterPickerAdapter = ArrayAdapter(requireContext(),
             android.R.layout.simple_spinner_dropdown_item,
-            viewModel.converters.map { it.name }
-        )
+            viewModel.converters.map { it.name })
 
-        binding.converterPicker.adapter = adapter
+        binding.converterPicker.adapter = converterPickerAdapter
 
-        binding.converterPicker.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                viewModel.changeConverter(position)
+        binding.converterPicker.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?, view: View?, position: Int, id: Long
+                ) {
+                    viewModel.changeConverter(position)
+                }
 
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
             }
 
-            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        binding.firstNumberUnit.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?, view: View?, position: Int, id: Long
+                ) {
+                    viewModel.setFirstNumberUnit(position)
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
+            }
+
+        binding.secondNumberUnit.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?, view: View?, position: Int, id: Long
+                ) {
+                    viewModel.setSecondNumberUnit(position)
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
+            }
+
+        viewModel.converter.observe(viewLifecycleOwner) {
+            val adapter = ArrayAdapter(
+                requireContext(),
+                android.R.layout.simple_spinner_dropdown_item,
+                viewModel.getUnitNames()
+            )
+            binding.firstNumberUnit.adapter = adapter
+            binding.secondNumberUnit.adapter = adapter
+            binding.secondNumberUnit.setSelection(1)
         }
 
+        viewModel.firstNumber.observe(viewLifecycleOwner) {
+            binding.firstNumber.removeTextChangedListener(firstNumberWatcher)
+            binding.firstNumber.setText(it.toString())
+            binding.firstNumber.addTextChangedListener(firstNumberWatcher)
+        }
+
+        viewModel.secondNumber.observe(viewLifecycleOwner) {
+            binding.secondNumber.removeTextChangedListener(secondNumberWatcher)
+            binding.secondNumber.setText(it.toString())
+            binding.secondNumber.addTextChangedListener(secondNumberWatcher)
+        }
+
+        binding.firstNumber.addTextChangedListener(firstNumberWatcher)
+        binding.secondNumber.addTextChangedListener(secondNumberWatcher)
+
         return binding.root
+    }
+
+    private val firstNumberWatcher = object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {}
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            val newValue = s?.toString()?.toDoubleOrNull() ?: 0.0
+            Log.i("ConverterFragment", "first new value ${newValue}")
+            viewModel.calculateSecondNumber(newValue)
+        }
+    }
+
+    private val secondNumberWatcher = object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {}
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            val newValue = s?.toString()?.toDoubleOrNull() ?: 0.0
+            Log.i("ConverterFragment", "second new value ${newValue}}")
+            viewModel.calculateFirstNumber(newValue)
+        }
     }
 }
--- app/src/main/java/ru/ac/uniyar/tyumentsev/unitconverter/ui/converter/ConverterViewModel.kt
+++ app/src/main/java/ru/ac/uniyar/tyumentsev/unitconverter/ui/converter/ConverterViewModel.kt
@@ -11,11 +11,87 @@ class ConverterViewModel : ViewModel() {
         _converter.value = converters[position]
     }
 
+    fun getUnitNames(): List<String> {
+        return _converter.value!!.units.map { it.name }
+    }
+
     val converters = listOf(
-        Converter("Converter 1", listOf(Unit("Unit 1", 1.0), Unit("Unit 2", 2.0))),
-        Converter("Converter 2", listOf(Unit("Unit 3", 3.0), Unit("Unit 4", 4.0)))
+        Converter(
+            "length",
+            listOf(
+                Unit("m", 1.0),
+                Unit("mm", 0.001),
+                Unit("cm", 0.01),
+                Unit("dm", 0.1),
+                Unit("km", 1000.0),
+                Unit("mile", 1609.35),
+                Unit("yard", 0.9144),
+                Unit("foot", 0.3048),
+                Unit("inch", 0.0254)
+            )
+        ),
+        Converter(
+            "weight",
+            listOf(
+                Unit("g", 1.0),
+                Unit("μg", 0.000001),
+                Unit("mg", 0.001),
+                Unit("kg", 1000.0),
+                Unit("cen", 100000.0),
+                Unit("ton", 1000000.0),
+                Unit("lb", 453.5922),
+                Unit("oz", 28.3495),
+            )
+        ),
+        Converter(
+            "area",
+            listOf(
+                Unit("m^3", 1.0),
+                Unit("mL", 1000000.0),
+                Unit("L", 1000.0),
+                Unit("pt", 2113.376),
+                Unit("kw", 1056.688),
+                Unit("gal", 1056.688),
+                Unit("ft^3", 35.315),
+                Unit("in^3", 1023.744),
+            )
+        ),
     )
     private val _converter = MutableLiveData<Converter>()
     val converter: LiveData<Converter>
         get() = _converter
+
+    private val _firstNumber = MutableLiveData<Double>()
+    val firstNumber: LiveData<Double>
+        get() = _firstNumber
+
+    fun calculateFirstNumber(number: Double) {
+        _firstNumber.value = number * _secondNumberUnit.value!!.value / _firstNumberUnit.value!!.value
+    }
+
+    private val _firstNumberUnit = MutableLiveData<Unit>()
+
+    val firstNumberUnit: LiveData<Unit>
+        get() = _firstNumberUnit
+
+    fun setFirstNumberUnit(position: Int) {
+        _firstNumberUnit.value = _converter.value!!.units[position]
+    }
+
+    private val _secondNumber = MutableLiveData<Double>()
+    val secondNumber: LiveData<Double>
+        get() = _secondNumber
+
+    fun calculateSecondNumber(number: Double) {
+        _secondNumber.value = number * _firstNumberUnit.value!!.value / _secondNumberUnit.value!!.value
+    }
+
+    private val _secondNumberUnit = MutableLiveData<Unit>()
+
+    val secondNumberUnit: LiveData<Unit>
+        get() = _secondNumberUnit
+
+    fun setSecondNumberUnit(position: Int) {
+        _secondNumberUnit.value = _converter.value!!.units[position]
+    }
 }
