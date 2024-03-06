89adc5d9ee5a63c8a4d1540bf35a1714116fbbc8
Dmitry Tyumentsev <tyumentsevde@gmail.com>
2024-03-06 23:29:14 +0300

    New Converter save state

---
+++ app/src/main/java/ru/ac/uniyar/tyumentsev/unitconverter/dto/UnitRow.kt
@@ -0,0 +1,7 @@
+package ru.ac.uniyar.tyumentsev.unitconverter.dto
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class UnitRow(val name: String?, val value:Double?): Parcelable
--- app/src/main/java/ru/ac/uniyar/tyumentsev/unitconverter/fragment/NewConverterFragment.kt
+++ app/src/main/java/ru/ac/uniyar/tyumentsev/unitconverter/fragment/NewConverterFragment.kt
@@ -1,5 +1,7 @@
 package ru.ac.uniyar.tyumentsev.unitconverter.fragment
 
+import android.icu.text.DecimalFormat
+import android.icu.text.NumberFormat
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -15,6 +17,7 @@ import ru.ac.uniyar.tyumentsev.unitconverter.R
 import ru.ac.uniyar.tyumentsev.unitconverter.database.AppDatabase
 import ru.ac.uniyar.tyumentsev.unitconverter.databinding.NewConverterFragmentBinding
 import ru.ac.uniyar.tyumentsev.unitconverter.dto.UnitDto
+import ru.ac.uniyar.tyumentsev.unitconverter.dto.UnitRow
 import ru.ac.uniyar.tyumentsev.unitconverter.repository.ConverterRepository
 import ru.ac.uniyar.tyumentsev.unitconverter.viewmodel.NewConverterViewModel
 import ru.ac.uniyar.tyumentsev.unitconverter.viewmodel.NewConverterViewModelFactory
@@ -26,15 +29,39 @@ class NewConverterFragment : Fragment() {
 
     private lateinit var binding: NewConverterFragmentBinding
 
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        val rowContainer = binding.rowContainer
+        if (savedInstanceState != null) {
+            val unitRows = savedInstanceState.getParcelableArrayList<UnitRow>("unitRows")
+            rowContainer.removeAllViews()
+            if (unitRows != null) {
+                for (value in unitRows) {
+                    val view =
+                        layoutInflater.inflate(R.layout.unit_row_fragment, rowContainer, false)
+                    val removeRowButton =
+                        view.findViewById<FloatingActionButton>(R.id.removeRowButton)
+                    this.writeRowUnit(view, value)
+                    removeRowButton.setOnClickListener {
+                        if (rowContainer.childCount > 2) {
+                            rowContainer.removeView(view)
+                        }
+                    }
+                    rowContainer.addView(view, rowContainer.childCount)
+                }
+            }
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = DataBindingUtil.inflate(
-        inflater,
-        R.layout.new_converter_fragment,
-        container,
-        false
+            inflater,
+            R.layout.new_converter_fragment,
+            container,
+            false
         )
         val application = requireNotNull(this.activity).application
         val dao = AppDatabase.getDatabase(application).converterDao()
@@ -57,7 +84,8 @@ class NewConverterFragment : Fragment() {
             var units = listOf<UnitDto>()
             val converterName = binding.converterNameEdit.text.toString().trim()
             if (converterName.isEmpty()) {
-                binding.converterNameEdit.error = context?.resources?.getString(R.string.empty_field_error_message)
+                binding.converterNameEdit.error =
+                    context?.resources?.getString(R.string.empty_field_error_message)
                 return@setOnClickListener
             }
             rowContainer.forEach { unit ->
@@ -67,14 +95,16 @@ class NewConverterFragment : Fragment() {
                     val unitNameField = unit.findViewById<EditText>(R.id.editUnit)
                     val unitName = unitNameField.text.toString().trim()
                     if (number == null) {
-                        unitNameField.error = context?.resources?.getString(R.string.empty_field_error_message)
+                        unitNameField.error =
+                            context?.resources?.getString(R.string.empty_field_error_message)
                         return@setOnClickListener
                     }
                     if (unitName.isEmpty()) {
-                        unitNameField.error = context?.resources?.getString(R.string.empty_field_error_message)
+                        unitNameField.error =
+                            context?.resources?.getString(R.string.empty_field_error_message)
                         return@setOnClickListener
                     }
-                    units = units.plus(UnitDto(name = unitName, value = 1/number))
+                    units = units.plus(UnitDto(name = unitName, value = 1 / number))
                 }
             }
             viewModel.createConverter(converterName, units)
@@ -82,5 +112,35 @@ class NewConverterFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+    private fun writeRowUnit(view: View, value: UnitRow?) {
+        val numberField = view.findViewById<EditText>(R.id.editNumber)
+        val unitNameField = view.findViewById<EditText>(R.id.editUnit)
+        if (value?.value != null && value.value != 0.0) {
+            val nf: NumberFormat = DecimalFormat("##.###")
+            numberField.setText(nf.format(value.value))
+        } else {
+            numberField.setText("")
+        }
+        unitNameField.setText(value?.name)
+    }
+
+    private fun readUnitRows(): List<UnitRow> {
+        var units = emptyList<UnitRow>()
+        binding.rowContainer.forEach {
+            val numberField = it.findViewById<EditText>(R.id.editNumber)
+            val number = numberField.text.toString().toDoubleOrNull()
+            val unitNameField = it.findViewById<EditText>(R.id.editUnit)
+            val unitName = unitNameField.text.toString().trim()
+            units = units.plus(UnitRow(name = unitName, value = number))
+        }
+        return units
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val unitRows = this.readUnitRows()
+        outState.putParcelableArrayList("unitRows", ArrayList(unitRows))
     }
 }
